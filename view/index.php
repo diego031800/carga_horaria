762116185c1ca4fcddef763799240bd7fd0c12fb
Gerald Ayala <geraldayala87@gmail.com>
2023-08-18 11:52:42 -0500

    Unidades y semestre

--- view/index.php
+++ view/index.php
@@ -16,8 +16,19 @@
     </form>
     <div class="container">
         <div class="row">
+
             <div class="col-12 row">
-                <h3>Unidad: AQUI VA LA UNIDAD</h3>
+                <h3>Registro de la carga horaria</h3>
+                <div class="col-6">
+                    <label for="" class="form-label">Semestre</label>
+                    <select name="" class="form-control" id="">
+                    </select>
+                </div>
+                <div class="col-6">
+                    <label for="" class="form-label">Unidad</label>
+                    <select name="" class="form-control" id="">
+                    </select>
+                </div>
                 <div class="col-6">
                     <label for="" class="form-label">Modo</label>
                     <select name="" class="form-control" id="">
@@ -26,24 +37,13 @@
                     </select>
                 </div>
                 <div class="col-6">
-                    <label class="form-label" for="ciclo">Programa</label>
+                    <label class="form-label" for="ciclo">Mencion</label>
                     <select name="" class="form-control" id="">
-                        <option value="pr1">Programa 1</option>
-                        <option value="pr2">Programa 2</option>
                     </select>
                 </div>
             </div>
             <div class="col-12 row">
                 <h3>Datos del curso</h3>
-                <div class="col-6">
-                    <label for="" class="form-label">Nombre del curso</label>
-                    <select name="cursoNombre" id="cursoNombre" class="form-control">
-                        <option value="Curso 1">Curso 1</option>
-                        <option value="Curso 2">Curso 2</option>
-                        <option value="Curso 3">Curso 3</option>
-                        <option value="Curso 4">Curso 4</option>
-                    </select>
-                </div>
                 <div class="col-6">
                     <label class="form-label" for="ciclo">Ciclo</label>
                     <select name="ciclo" class="form-control" id="ciclo">
@@ -53,6 +53,15 @@
                         <option value="4">4</option>
                         <option value="5">5</option>
                         <option value="6">6</option>
+                    </select>
+                </div>
+                <div class="col-6">
+                    <label for="" class="form-label">Nombre del curso</label>
+                    <select name="cursoNombre" id="cursoNombre" class="form-control">
+                        <option value="Curso 1">Curso 1</option>
+                        <option value="Curso 2">Curso 2</option>
+                        <option value="Curso 3">Curso 3</option>
+                        <option value="Curso 4">Curso 4</option>
                     </select>
                 </div>
                 <div class="col-6">
@@ -72,7 +81,7 @@
                 <div class="col-6">
                     <label for="" class="form-label">Acciones</label><br>
                     <button class="btn btn-success" onClick="agregar();">Agregar</button>
-                    <input type="text" hidden>
+                    <input type="text" hidden id="cursoEditar">
                     <button id="guardar" class="btn btn-warning" onClick="guardar();" disabled="true">Guardar</button>
                 </div>
             </div>
@@ -101,88 +110,16 @@
             </tbody>
         </table>
     </div>
-        
+
 
 
     <script src="../assets/js/jquery-3.7.0.min.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
-    <script src="../view/js/index.js"></script>
     <script src="../assets/js/datepicker/bootstrap-datepicker.min.js"></script>
     <script src="../assets/js/datepicker/bootstrap-datepicker.es.min.js"></script>
     <script src="../assets/js/datepicker/bootstrapValidator.min.js"></script>
     <script src="../assets/js/datepicker/es_ES.min.js"></script>
-    <script>
-    $(document).ready(function() {
-        $('.registerFormFcMv').bootstrapValidator({
-            live: 'enabled',
-            fields: {
-                newinputTratFechaIni: {
-                    validators: {
-                        date: {
-                            format: 'DD/MM/YYYY',
-                            message: 'ESTE VALOR NO COINCIDE CON UNA FECHA'
-                        },
-                        stringLength: {
-                            min: 10,
-                            max: 10,
-                            message: 'LA LONGITUD MÁXIMA ES DE 10 INCLUYENDO /'
-                        },
-                        regexp: {
-                            regexp: /^[0-9-/]+$/,
-                            message: 'LA FECHA SOLO PUEDE TENER NÚMEROS Y /'
-                        }
-                    }
-                }
-            }
-        });
-
-        $('.datepicker3').datepicker({
-            container: '.container-calendar',
-            autoclose: true,
-            todayHighlight: true,
-            calendarWeeks: true,
-            format: 'dd/mm/yyyy',
-            language: 'es',
-            multidate: true
-        });
-    });
-    </script>
-    <script>
-        function ver(){
-            var text = document.getElementById("newTratFechaIni").value;
-            alert(text);
-        }
-    </script>
-    <script>
-        var listacursos=[{"curso":"Curso 1", "horas": 64}];
-        function agregar(){
-            var cursonombre = document.getElementById("cursoNombre").value;
-            var cursohoras = document.getElementById("cursoHoras").value;
-            if (cursonombre == "" || cursohoras==""){
-                alert("No deben haber campos vacíos");
-                return;
-            }
-            listacursos.push({"curso" : cursonombre,"horas" : cursohoras});
-            let index = listacursos.length -1;
-            fila =  '<tr><th scope="row">'+cursonombre+'</th><td>'+cursohoras+'</td>'+
-            '<td><button class="btn btn-info" onClick="editar('+index+');">Editar</button><button class="btn btn-danger">Eliminar</button></td>'+
-            '<td>Nombre del docente</td>'+
-            '<td><button class="btn btn-danger">Ver</button></td></tr>';
-            $("#cursosTabla").append(fila);
-        }
-        function editar(index){
-            $('#cursoNombre').val(listacursos[index].curso)
-            $('#cursoHoras').val(listacursos[index].horas)
-            $('#cursoHoras').val(listacursos[index].horas)
-            document.getElementById('guardar').disabled = false;
-        }
-        function guardar(index){
-            $('#cursoNombre').val(listacursos[index].curso)
-            $('#cursoHoras').val(listacursos[index].horas)
-            $('#cursoHoras').val(listacursos[index].horas)
-            document.getElementById('guardar').disabled = false;
-        }
-        
-    </script>
+    <script src="../view/js/index.js"></script>
 </body>
+
 </html>
--- view/js/index.js
+++ view/js/index.js
@@ -2,27 +2,95 @@
 let array_unidades = [];
 
 // FUNCIONES
-function get_unidades()
-{
-  let opcion = 'get_unidades';
+function get_unidades() {
+  let opcion = "get_unidades";
   $.ajax({
     type: "POST",
-    url: '../../../carga_horaria/controllers/main/CargaHorariaController.php',
-    data: 'opcion='+opcion,
+    url: "../../../carga_horaria/controllers/main/CargaHorariaController.php",
+    data: "opcion=" + opcion,
     success: function (data) {
       array_unidades = JSON.parse(data);
       console.log(array_unidades);
     },
-    error:function (data) 
-    { 
-      alert('Error al mostrar');
-    }
+    error: function (data) {
+      alert("Error al mostrar");
+    },
   });
 }
 
-function load_document()
-{
+function load_document() {
   get_unidades();
+}
+
+$(document).ready(function() {
+  $('.registerFormFcMv').bootstrapValidator({
+      live: 'enabled',
+      fields: {
+          newinputTratFechaIni: {
+              validators: {
+                  date: {
+                      format: 'DD/MM/YYYY',
+                      message: 'ESTE VALOR NO COINCIDE CON UNA FECHA'
+                  },
+                  stringLength: {
+                      min: 10,
+                      max: 10,
+                      message: 'LA LONGITUD MÁXIMA ES DE 10 INCLUYENDO /'
+                  },
+                  regexp: {
+                      regexp: /^[0-9-/]+$/,
+                      message: 'LA FECHA SOLO PUEDE TENER NÚMEROS Y /'
+                  }
+              }
+          }
+      }
+  });
+
+  $('.datepicker3').datepicker({
+      container: '.container-calendar',
+      autoclose: true,
+      todayHighlight: true,
+      calendarWeeks: true,
+      format: 'dd/mm/yyyy',
+      language: 'es',
+      multidate: true
+  });
+});
+
+var listacursos = [{ curso: "Curso 1", horas: 64 }];
+function agregar() {
+  var cursonombre = document.getElementById("cursoNombre").value;
+  var cursohoras = document.getElementById("cursoHoras").value;
+  if (cursonombre == "" || cursohoras == "") {
+    alert("No deben haber campos vacíos");
+    return;
+  }
+  listacursos.push({ curso: cursonombre, horas: cursohoras });
+  let index = listacursos.length - 1;
+  fila =
+    '<tr><th scope="row">' +
+    cursonombre +
+    "</th><td>" +
+    cursohoras +
+    "</td>" +
+    '<td><button class="btn btn-info" onClick="editar(' +
+    index +
+    ');">Editar</button><button class="btn btn-danger">Eliminar</button></td>' +
+    "<td>Nombre del docente</td>" +
+    '<td><button class="btn btn-danger">Ver</button></td></tr>';
+  $("#cursosTabla").append(fila);
+}
+function editar(index) {
+  $("#cursoNombre").val(listacursos[index].curso);
+  $("#cursoHoras").val(listacursos[index].horas);
+  document.getElementById("guardar").disabled = false;
+}
+function guardar() {
+  let index = document.getElementById("cursoEditar").value;
+  listacursos[index].curso = document.getElementById("cursoNombre").value;
+  listacursos[index].horas = document.getElementById("cursoHoras").value;
+  $("#cursoHoras"). val("");
+  document.getElementById("guardar").disabled = true;
 }
 
 // EVENTOS
