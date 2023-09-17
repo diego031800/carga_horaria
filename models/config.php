025bf79775891dff8c1c2f3736f9448a8af25c28
Jose Azabache Santos <aspirojose@gmail.com>
2023-09-17 12:48:25 -0500

    Nueva interfaz

--- models/config.php
+++ models/config.php
@@ -2,7 +2,7 @@
 
     // CONFIGURACION SQL SERVER -----  GERALD  ------
     define('USER_NAME_SQL_SERVER', 'sa');
-    define('PASS_SQL_SERVER', '1478963sws**');
+    define('PASS_SQL_SERVER', '16650039');
     define('HOST_SQL_SERVER', 'localhost');
     define('DB_NAME_SQL_SERVER', 'is_posgrado');
     //  define('USER_NAME_SQL_SERVER', 'sa');
