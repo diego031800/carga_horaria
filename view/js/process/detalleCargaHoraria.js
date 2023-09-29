74baea0beb104407d0caf201fd14998602698b89
Jose Azabache Santos <aspirojose@gmail.com>
2023-09-29 17:07:15 -0500

    Correos docentes

--- view/js/process/detalleCargaHoraria.js
+++ view/js/process/detalleCargaHoraria.js
@@ -121,6 +121,7 @@ function buscar() {
         ],
         responsive: false,
         select: true,
+        lengthMenu: [5, 10, 15, 20, 25],
         columnDefs: [
           {
               targets: -1,
