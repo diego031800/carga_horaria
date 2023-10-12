3ce6cd1572833ab4ef1bbc04083fb204dfc7fb15
Gerald <geraldayala87@gmail.com>
2023-10-12 00:59:42 -0500

    Todo ok

--- controllers/main/CorreosController.php
+++ controllers/main/CorreosController.php
@@ -16,29 +16,13 @@ $datosJson = json_decode($datos);
 $itemsEnviados = array();
 $datosEnvio = new datosEnvio();
 
-
-
-function timer_diff($timeStart)
-{
-    return number_format(microtime(true) - $timeStart, 3);
-}
-
 try {
     foreach ($datosJson as $item) {
-        $timeStart1 = microtime(true);
         $correo = new CorreoCargaHoraria();
         $pdf = new CredencialDocente();
-        error_log("INICIO: ".timer_diff($timeStart1));
-        $timeStart1 = microtime(true);
         $rutaPdf = $pdf->generarCredencial($item->nombre,$item->codigo,$item->documento,$item->sem);
-        error_log("GENERAR PDF: ".timer_diff($timeStart1));
-        $timeStart1 = microtime(true);
         $itemEnviado = $correo->enviarCredencial($item,$rutaPdf); 
-        error_log("ENVIAR CORREO: ".timer_diff($timeStart1));
-        $timeStart1 = microtime(true);
         $itemsEnviados[] = $itemEnviado;
-        error_log("FIN: ".timer_diff($timeStart1));
-        $timeStart1 = microtime(true);
         unlink($rutaPdf);
         $correo->cerrarConexion();
     }
