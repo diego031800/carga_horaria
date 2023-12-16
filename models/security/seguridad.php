309c3178825ba9d48908dd121db080655591cb64
Gerald Ayala <geraldayala87@gmail.com>
2023-12-16 14:04:35 -0500

    Avance

--- models/security/seguridad.php
+++ models/security/seguridad.php
@@ -12,25 +12,37 @@ class Seguridad
         $this->con = new connection();
     }
 
-    public function opciones($parametros){
+    public function opciones($parametros)
+    {
         $this->parametros = $parametros;
         switch ($this->parametros['opcion']) {
             case 'value':
                 # code...
                 break;
-            
+
             default:
                 # code...
                 break;
         }
     }
 
-    private function get_usuarios(){
+    private function get_usuarios()
+    {
         try {
-            //code...
+            $sql = "SELECT 
+            U.usu_id, 
+            U.usu_login,
+            PER.per_nombres + ' ' + PER.per_ape_paterno + ' ' + PER.per_ape_materno AS nombres
+          FROM SISTEMA.USUARIO U 
+          INNER JOIN SISTEMA.PERSONA PER ON PER.per_id = U.per_id
+          WHERE U.usu_estado = 1";
+            $datos = $this->con->return_query_sqlsrv($sql);
+            $usuarios = array();
+            while ($row = $datos->fetch(PDO::FETCH_ASSOC)) {
+                $usuario = $row['usuario'];
+            }
         } catch (Exception $ex) {
-            return json_encode(['respuesta' => 0, 'mensaje' => $this->con->error_mysql().$ex]);
+            return json_encode(['respuesta' => 0, 'mensaje' => $ex]);
         }
     }
-
 }
