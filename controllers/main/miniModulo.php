6dbf08ca2213a58cd35a5ecaab258f093befd2a1
Gerald Eduardo Ayala Mendoza <geraldayala87@gmail.com>
2024-08-13 12:39:37 -0500

    Cambio del video

--- controllers/main/miniModulo.php
+++ controllers/main/miniModulo.php
@@ -81,7 +81,7 @@ function generarMensajeCorreo()
     $mensaje .= '<ul><li>Anderson J. Zavaleta Simón / UTIC-EPG: 984 599 249</li>';
     $mensaje .= '<li>O puedes unirte al Grupo de Docentes de la EPG; Unete aquí:  : <a href="https://chat.whatsapp.com/EqKTfbg0XG1H1aWjpNLwsC ">Grupo de WhatsApp</a></li></ul>';
     $mensaje .= '<p><a href="http://www.epgnew.unitru.edu.pe">www.epgnew.unitru.edu.pe</a></p>';
-    $mensaje .= '<p>Video tutorial para el proceso de registro de notas online: <a href="https://drive.google.com/file/d/150q2t4Wo3k7RH_5L0UE9qaK4WiGjdtvB/view?usp=drive_link">Enlace al video</a></p>';
+    $mensaje .= '<p>Video tutorial para el proceso de registro de notas online: <a href="https://drive.google.com/file/d/1TUADa4xMJZF7cZDI65DlA9beSyxj4Rn4/view?usp=sharing">Enlace al video</a></p>';
     $mensaje .= '<p>Estimado Docente de la Escuela de Posgrado ingrese en el siguiente enlace para descargar el Manual del Docente para el uso del sistema de la EPG/UNT: <a href="https://drive.google.com/file/d/1xY3-vfL527XFuIZloVGVMFn8rYBwLFIP/view?usp=sharing">Manual SIGAP</a></p>';
     $mensaje .= '<p>Nota: Sigap es únicamente para la gestión académica, en la cual podrá registrar las asistencias y notas online, así como la validación de actas de los cursos.</p>';
     $mensaje .='<p>ATTE. Unidad de Tecnologías Informáticas y Comunicaciones de la EPG.</p>';
